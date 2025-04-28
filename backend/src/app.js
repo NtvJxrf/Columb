4d4bf7f1b2bedc219d9e436fbba123ad1f045005
artemy <artemy.nokhrin@yandex.ru>
2025-04-28 16:45:17 +0500

    add

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,10 +23,7 @@ app.use(
   })
 )
 app.use(helmet())
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
