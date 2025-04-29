318ed0df717fe07716198da36e2a1b8689e6bbbb
artemy <artemy.nokhrin@yandex.ru>
2025-04-29 12:38:24 +0500

    add

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,7 +30,7 @@ app.use(cors({
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.set('trust proxy', true);
+app.set('trust proxy', 'loopback');
 app.use('/', routes)
 // Error handling
 app.use(errorConverter)
